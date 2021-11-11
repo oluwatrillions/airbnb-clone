bc759051a624747e2b287814dd23f194b56cac39
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2021-11-11 10:45:36 +0100

    heart Images update

--- src/SearchResult.js
+++ src/SearchResult.js
@@ -4,11 +4,18 @@ import './SearchResult.css'
 function SearchResult({src, location, price, distance}) {
     return (
         <div className='edited'>
-            <img src={src} alt='' />
-            <div className='desc'>
-                <h3>{location} <span>{ price}</span></h3>
-                <h3>{ distance}</h3>
+            <div className='imgsDiv'>
+                <img src={src} alt='' />
+                <img className='iheart' src="https://img.icons8.com/material-two-tone/50/000000/like--v1.png" alt='heart'/>
             </div>
+             <div className='infoRoom'>
+                <h3>{location}</h3>
+                <h3>{ price}</h3>
+            </div>
+            <div className='distance'>
+                <h3>{distance}</h3>
+            </div>
+           
         </div>
     )
 }
